bfae96f2cf0bf8f124dbfd826131a53dc458f11b
Yigit Egilmez <m.yigit.egilmez@gmail.com>
2016-11-22 15:15:53 +0800

    Finish user edit, update, index and destroy actions

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+  def setup 
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {user: { name: "", email: "invalid@invalid", password: "", password_confirmation: "12"}}
+    assert_template 'users/edit' 
+  end
+
+  test "successful edits" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Michael Test User"
+    email = "m.yigit.egilmez@gmail.com"
+    patch user_path(@user), params: { user: {name: name , email: email, 
+                                             password:"", password_confirmation:""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email                                           
+  end
+
+end
