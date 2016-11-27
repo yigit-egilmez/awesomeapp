165515520cc4b95e4dc8fddd4c121ed09d5bb162
Yigit Egilmez <m.yigit.egilmez@gmail.com>
2016-11-27 19:35:52 +0800

    access

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,16 +1,16 @@
-#ENV['S3_BUCKET'] 
-#ENV['S3_ACCESS_KEY']
-#ENV['S3_SECRET_KEY']
+#
+#
+#
 
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = 'micropost-images-us-standard'
+    config.aws_bucket = ENV['S3_BUCKET'] 
     config.aws_acl    = :public_read
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
     
     config.aws_credentials = {
-        access_key_id:     'AKIAIW5JWKEYUYTZZMDA',
-        secret_access_key: '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' ,
+        access_key_id:     ENV['S3_ACCESS_KEY'],
+        secret_access_key: ENV['S3_SECRET_KEY'] ,
         region:            'us-east-1' # Required
     }
     
