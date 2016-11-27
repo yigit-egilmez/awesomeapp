84a57e4abfc921ba147ae96c85a4f7b47aefe509
Yigit Egilmez <m.yigit.egilmez@gmail.com>
2016-11-27 19:19:26 +0800

    S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,20 @@
-if Rails.env.production?
+#ENV['S3_BUCKET'] 
+#ENV['S3_ACCESS_KEY']
+#ENV['S3_SECRET_KEY']
+
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    config.storage    = :aws
+    config.aws_bucket = 'micropost-images-us-standard'
+    config.aws_acl    = :public_read
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    
+    config.asset_host = 'http://awesomeapp-env.8pmmyucsp2.us-west-2.elasticbeanstalk.com/'
+
+    config.aws_credentials = {
+        provider:          'AWS',
+        access_key_id:     'AKIAIW5JWKEYUYTZZMDA',
+        secret_access_key: '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' ,
+        region:            'us-east-1' # Required
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    
   end
-end
